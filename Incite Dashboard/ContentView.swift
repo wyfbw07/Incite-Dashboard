a660a9ef6a6d537e99dd770f0b43d48666f4aacd
Yifan Wang <wyfbw07@gmail.com>
2023-12-28 21:02:53 -0500

    Initial Commit

---
+++ Incite Dashboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Incite Dashboard
+//
+//  Created by Yifan Wang on 12/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Incite Dashboard/Incite_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Incite_DashboardApp.swift
+//  Incite Dashboard
+//
+//  Created by Yifan Wang on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Incite_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
